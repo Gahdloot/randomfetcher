dc4f93b6614e4db62d8eff560ff8518a28c5969b
gahdloot <davidlearn27@gmail.com>
2022-03-31 10:30:22 +0100

    The random fetcher project

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print('hello git')
+import random
+
+hii = []
+
+
+#this Function splits the user input and use them to generate its figures
+def RanGenrator(x):
+    l = x // 2
+    b = random.randint(1, l)
+    xr = l - b
+    c = random.randint(0, xr)
+    xr = b - c
+    d = random.randint(0, xr)
+    o = b + c + d
+    i = x - o
+    hii.append(b)
+    hii.append(c)
+    hii.append(d)
+    hii.append(i)
+    #print(f'your numbers are {b},{c},{d},{i} which make up {x}')
+
+
+'''def randi():
+    w = random.choice(hii)
+    print(w)
+    hii.remove(w)'''
+
+
+
+try:
+    rr = int(input('Enter a random number from 20 - 100 : '))
+    RanGenrator(rr)
+    print(hii)
+    print(f'That Makes up {rr}')
+
+except ValueError :
+    print('Invalid Input Try Again')
+
+
+except IndexError:
+    print('ouch!! code crash try again!!!')
+
